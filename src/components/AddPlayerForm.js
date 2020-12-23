fe0b78cf19fa62b528137d15121acfb8ccbd206e
jonsax6 <jdc.saxjunky@gmail.com>
2020-12-23 18:11:03 -0500

    add player feature, made components modular js files

---
+++ src/components/AddPlayerForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class AddPlayerForm extends Component {
+
+    state = {
+        value: ''
+    };
+
+    handleValueChange = (e) => {
+        this.setState({ value: e.target.value });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addPlayer(this.state.value);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.handleValueChange}
+                    placeholder="Enter a players name"
+                />
+
+                <input 
+                    type="submit"
+                />
+            </form>
+        );
+    }
+}
+
+export default AddPlayerForm;
